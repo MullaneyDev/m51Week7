a2b301410a98a527ab660c51e43d0bfa63de8d99
MullaneyDev <matt.mullaney@live.co.uk>
2023-10-13 11:12:07 +0100

    Addition of cors to allow connection to front end

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors")
 
 require("./db/connection"); //not in a variable to enable the connection to stay open and not require a function call
 const bookRouter = require("./books/routes");
 const authorRouter = require("./author/routes");
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 app.use(bookRouter);
